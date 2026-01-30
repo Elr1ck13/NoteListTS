d52d74384eb68478378fe74b24487a5e8b49092c
Elr1ck13 <renewcod13@outlook.com>
2026-01-30 06:47:44 +0000

    feat: inicializar estructura base de la aplicación en src

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Notes App</h1>
+      <p>You can put your notes or task for today.</p>
+    </div>
+  );
+};
+
+export default App;
